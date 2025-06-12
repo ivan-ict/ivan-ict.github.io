e52e0aac5f3c4c96883f1a609b7ef0d7551fe85e
ivan <124525312+ivan-ict@users.noreply.github.com>
2025-06-12 16:54:32 +1000

    add footer

--- app/icons/tilted-grid.tsx
+++ app/icons/tilted-grid.tsx
@@ -13,10 +13,10 @@ export default function TiltedGrid() {
           <path d="M.5 56V.5H72" fill="none"></path>
         </pattern>
       </defs>
-      <rect width="100%" height="100%" stroke-width="0" fill="url(#pt)"></rect>
+      <rect width="100%" height="100%" strokeWidth="0" fill="url(#pt)"></rect>
       <svg x="50%" y="16" className="overflow-visible">
-        <rect stroke-width="0" width="73" height="57" x="0" y="56"></rect>
-        <rect stroke-width="0" width="73" height="57" x="72" y="168"></rect>
+        <rect strokeWidth="0" width="73" height="57" x="0" y="56"></rect>
+        <rect strokeWidth="0" width="73" height="57" x="72" y="168"></rect>
       </svg>
     </svg>
   );
