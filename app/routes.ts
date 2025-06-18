3c076803807fa6ecce37ccc383f7c35deffdfc74
ivan <124525312+ivan-ict@users.noreply.github.com>
2025-06-18 16:34:10 +1000

    fix config & add 404 (#15)
    - ssr + nojekyll config
    - gh-pages deploy config

--- app/routes.ts
+++ app/routes.ts
@@ -9,6 +9,7 @@ export default [
   layout("layout.tsx", [
     index("routes/home.tsx"),
     route("blog", "routes/blog.tsx"),
+    route("*", "routes/404.tsx"),
     // route("blog", "routes/snippets.tsx"),
   ]),
 ] satisfies RouteConfig;
